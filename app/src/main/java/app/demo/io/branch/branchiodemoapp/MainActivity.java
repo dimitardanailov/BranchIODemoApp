c85709d04e66287fe2f449413c4a95e747b1a2ee
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-06-12 16:14:12 +0300

    Repo is able to connect Branch io server

---
+++ app/src/main/java/app/demo/io/branch/branchiodemoapp/MainActivity.java
@@ -0,0 +1,50 @@
+package app.demo.io.branch.branchiodemoapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import io.branch.referral.Branch;
+import io.branch.referral.BranchError;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Branch init
+        Branch.getInstance().initSession(new Branch.BranchReferralInitListener() {
+            @Override
+            public void onInitFinished(JSONObject referringParams, BranchError error) {
+                if (error == null) {
+                    // params are the deep linked params associated with the link that the user clicked -> was re-directed to this app
+                    // params will be empty if no data found
+                    // ... insert custom logic here ...
+                    Log.d(TAG, referringParams.toString());
+                    Log.d(TAG, "Branch io success");
+                } else {
+                    Log.d(TAG, "Branch io error");
+                    Log.d(TAG, error.getMessage());
+                }
+            }
+        }, this.getIntent().getData(), this);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        this.setIntent(intent);
+    }
+
+}
